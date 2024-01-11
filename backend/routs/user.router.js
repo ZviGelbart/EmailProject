c2b3b7c8f72fd7a7b970d82b958bac295110d2f5
dudi heber <dudiheber97@gmail.com>
2024-01-11 11:41:03 +0200

    add function in router

--- backend/routs/user.router.js
+++ backend/routs/user.router.js
@@ -2,6 +2,16 @@ const express = require("express");
 const userRouter = express.Router();
 const userServices = require("../services/user.services");
 
+userRouter.get("/", async function (req, res) {
+    let data = await userServices.getAllUser();
+    res.send(data);
+  });
 
+  
+
+userRouter.delete("/:id", function (req, res) {
+    userServices.deleteUser()
+    res.send("user");
+  });
 
 module.exports = userRouter;
