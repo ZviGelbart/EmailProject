9d09d9ac98f27bd0b6d3a7e032a88d5b21361216
dudi heber <dudiheber97@gmail.com>
2024-01-15 17:02:18 +0200

    add back

--- backend/routs/email.router.js
+++ backend/routs/email.router.js
@@ -1,13 +1,16 @@
 const express = require("express");
 const emailRouter = express.Router();
+const jwt = require('jsonwebtoken')
+const bcrypt = require('bcrypt')
 const emailServices = require("../services/email.services");
 
 
 // all inbox emails
 emailRouter.get("/inbox", async function(req,res){
-    const email = "dudi@gmail.com"
+    const email = "shlmi945679@gmail.com"
     let data= await emailServices.getAllEmails({destination: email})
     res.send(data)
+
 })
 
 emailRouter.get("/inbox/:email", async function(req,res){
